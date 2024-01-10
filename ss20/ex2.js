7ec9dd1121be97491f82c61f60922411f4b296f0
trinhkhachung <139752363+trinhkhachung@users.noreply.github.com>
2024-01-10 16:11:48 +0700

    Add files via upload

---
+++ ss20/ex2.js
@@ -0,0 +1,9 @@
+let i = 10
+console.log('giá trị i là :',i)
+let f = 20.5
+console.log('giá trị f là :', f)
+let x = 100 / "Apple";
+isNaN(x)
+console.log('giá trị x là :', x)
+let a = "apple"
+console.log('',a)
---
+++ ss20/ex3.js
@@ -0,0 +1,4 @@
+let a = +prompt('nhập chiều dài hcn')
+let b = +prompt('nhập chiều rộng hcn')
+let sum = a * b
+document.write('diện tích hcn là :',sum)
